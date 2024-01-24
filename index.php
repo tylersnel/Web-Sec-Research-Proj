174356cd963cab17d32b675398e83ab0b341e44a
tylersnel <snelgrove86@gmail.com>
2024-01-23 21:24:02 -0700

    first commit with basic login and db

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Page Title</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="app.css">
+<style>
+
+</style>
+<script src=""></script>
+<body>
+ 
+<!-- <img src="img_la.jpg" alt="LA" style="width:100%"> -->
+
+<div class="login-container">
+    <h1>Smaug Bank</h1>
+    <h2 class="main-login">Online Banking Login</h2>
+
+
+    <form action="login.php" method="post" class="login-form">
+        <input type="text" name="uname" class="form-input" placeholder="Username">
+        <input type="password" name="password" class="form-input" placeholder="Password">
+        <button type="submit" class="login-btn">Login</button>
+      </form>
+
+    <p>Don't have an account? <a href="signup.html">Sign up here</a>.</p>
+</div>
+
+<div class="quote">
+    <h1>"My armour is like tenfold shields, my teeth are swords, my claws spears, the shock of my tail is a thunderbolt, my wings a hurricane, and my breath death!"</h1>
+    <h1> Smaug, Founder and CEO</h1>
+</div>
+
+<script src="app.js"></script>
+</body>
+</html>
