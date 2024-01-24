acf97e886ea9fb70817070e6de1eae70785138e3
tylersnel <snelgrove86@gmail.com>
2024-01-24 16:08:16 -0700

    Create account page

---
+++ signup.php
@@ -0,0 +1,75 @@
+<?php 
+session_start();
+include "db_conn.php";
+
+  if (isset($_POST['submit'])) {
+
+    $uname = $_POST['user_name'];
+
+    $pass = $_POST['password'];
+
+    $name = $_POST['name'];
+
+
+    $sql = "INSERT INTO `users`(`user_name`, `password`, `name`) VALUES ('$uname','$pass','$name')";
+
+    $result = $conn->query($sql);
+
+    if ($result == TRUE) {
+
+      echo "New record created successfully.";
+
+    }else{
+
+      echo "Error:". $sql . "<br>". $conn->error;
+
+    } 
+
+    $conn->close(); 
+
+  }
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<h2>Create an Account</h2>
+
+<form action="" method="POST">
+
+  <fieldset>
+
+    <legend>User information:</legend>
+
+    User name:<br>
+
+    <input type="text" name="user_name">
+
+    <br>
+
+    Password:<br>
+
+    <input type="text" name="password">
+
+    <br>
+
+    First Name on Account:<br>
+
+    <input type="text" name="name">
+
+    <br>
+
+
+    <input type="submit" name="submit" value="submit">
+
+  </fieldset>
+
+</form>
+
+</body>
+
+</html>
