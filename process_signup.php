5e60b01925d20aeb36de77f64d574d28c9973008
Chapman-John <52362698+Chapman-John@users.noreply.github.com>
2024-02-01 14:03:58 -0800

    Update process_signup.php

--- process_signup.php
+++ process_signup.php
@@ -1,42 +1,27 @@
 <?php
-// if (empty($_POST['user_name'])) {
-    // die("Location: index.php?error= User Name is required");
-    // echo "User Name is required";
-//     header("Location: signup.php");
-//     exit();
-// }
-
-// if (empty($_POST['password'])) {
-//     die("Location: index.php?error= Password is required");
-// }
-
-// if (empty($_POST['password_repeat'])) {
-//     die("Location: index.php?error= Password Repeat is required");
-// }
-
-// if (empty($_POST['name'])) {
-//     die("Location: index.php?error= First Name on Account is required");
-// }
-
-//    // if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
-//    //     die("password must have a letter");
-//    // }
-
-//    if ($_POST["password"] !== $_POST["password_repeat"]) {
-//      die("passwords do not match. try again");
-//    }
 
 session_start();
 include "db_conn.php";
 
 
-if (isset($_POST['submit'])) {
+if (isset($_POST["submit"])) {
 
-    $uname = $_POST['user_name'];
+    $uname = $_POST["user_name"];
     // $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);  // Hash the password
-    $pass = $_POST['password'];
-    $name = $_POST['name'];
+    $pass = $_POST["password"];
+    $password_repeat = $_POST["password_repeat"];
+    $name = $_POST["name"];
     $account_total = 1000;
+
+
+
+   // if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
+   //     die("password must have a letter");
+   // }
+
+   // if ($_POST["password"] !== $_POST["password_repeat"]) {
+   //   die("passwords do not match. try again");
+   // }
 
     $stmt = $conn->prepare("INSERT INTO `users`(`user_name`, `password`, `name`, `account_total`) VALUES (?, ?, ?, ?)");
     $stmt->bind_param("ssss", $uname, $pass, $name, $account_total);
