8bf3ccb7a5ef7de1466b9626c2cf5a5041bfc19b
tylersnel <snelgrove86@gmail.com>
2024-02-03 09:36:42 -0700

    Created bill pay page and updated home.php

--- bill_pay.php
+++ bill_pay.php
@@ -26,55 +26,39 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
      <html>
 
      <head>
-          <title>HOME</title>
+          <title>Pay Bills</title>
           <link rel="stylesheet" type="text/css" href="style.css">
      </head>
 
      <body>
-          <h1>Account For <?php echo $_SESSION['name'] ?> </h1>
-          <br></br>
-          <ul>
-          <li><a href="transfer_funds.php">Transfer Funds</a></li>
-          <li><a href="bill_pay.php">Pay Bills</a></li>
-          </ul>
-          <br></br>
+          <h1>Pay Bills for  <?php echo $_SESSION['name'] ?></h1>
+
           <h2> Account Total <?php echo $accountTotal; ?></h2>
                 
-          <table border='1'>
-               <tr>
-                    <th>Transaction ID</th>
-                    <th>Date</th>
-                    <th>Amount</th>
-               </tr>
-               <?php while ($row = $result->fetch_assoc()) { ?>
-                    <tr>
-                         <td><?php echo $row['transactionID']; ?></td>
-                         <td><?php echo $row['date']; ?></td>
-                         <td><?php echo $row['amount']; ?></td>
-                    </tr>
-               <?php } ?>
-          </table>
-          <br></br>
-
           <form action="process_transaction.php" method="post">
+                <dv>From: <?php echo $_SESSION['name'] ?></dv>
+                <br>
+ 
+               <label for="amount_to">To:</label>
+               <input type="text" id="amount_to" name="amount_To" required>
+               <br> 
+
                <label for="amount">Amount:</label>
                <input type="number" id="amount" name="amount" required>
                <br>
 
-               <label for="date">Date:</label>
+               <label for="date">Check Date:</label>
                <input type="date" id="date" name="date" required>
                <br> 
 
-               <label for="transactionType">Select Transaction Type:</label>
-               <select id="transactionType" name="transactionType">
-               <option value="deposit">Deposit</option>
-               <option value="withdrawal">Withdrawal</option>
-               </select>
-               <br>
+               <label for="subject">Subject:</label>
+               <input type="text" id="subject" name="subject" required>
+               <br> 
 
                <!-- Assuming you have a session variable for the user ID -->
                <input type="hidden" name="account_id" value="<?php echo $_SESSION['id']; ?>">
                <input type="hidden" name="account_total" value="<?php echo $accountTotal; ?>">
+               <input type="hidden" name="transactionType" value= "<?php echo 'withdrawal';?>">
 
                <input type="submit" value="Submit">
           </form>
@@ -83,20 +67,10 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
                $successMessage = $_GET['success'];
                echo "<div style='color: green;'>$successMessage</div>";
           }
-
-          $errors = array();
-          if ($_SESSION['account_total'] < 0) {
-               array_push($errors, "Overdrawn Account!");
-               if (count($errors) > 0) {
-                    foreach ($errors as  $error) {
-                    echo "<div class='error' style='color: red;'>$error</div>";
-                    }
-               }
-          }
           ?>
 
           <br></br>
-          <a href="logout.php">Logout</a>
+          <a href="home.php">Return Home</a>
      </body>
 
      </html>
