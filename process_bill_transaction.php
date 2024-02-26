db2bdda86ad8c3d2ca2fcc5ab65e7b4d0bf344bb
tylersnel <67352917+tylersnel@users.noreply.github.com>
2024-02-26 06:29:51 -0700

    Added process_bill_transaction.php
    Processes bill paying transactions

---
+++ process_bill_transaction.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+include "db_conn.php"; // Include your database connection script
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $amount = $_POST['amount'];
+    $transactionDate = $_POST['date'];
+    $accountId = $_POST['account_id'];
+    $account_total = $_POST['account_total'];
+    $transaction_type=$_POST['transactionType'];
+    $amount_to = htmlspecialchars($_POST['amount_to']); //XSS attack prevention
+
+    // Validate form data (you can add more validation as needed)
+    if (empty($amount) || empty($transactionDate) || empty($accountId)) {
+        header("Location: transaction_form.php?error=All fields are required");
+        exit();
+    }
+    //If withdrawling from bank account
+    if($transaction_type == 'withdrawal'){
+        $amount*=-1;
+    }
+
+    // SQL to insert the transaction into the 'transactions' table
+    $insertQuery = "INSERT INTO transactions (amount, date, accountid) 
+                    VALUES ('$amount', '$transactionDate', '$accountId')";
+
+    if (mysqli_query($conn, $insertQuery)) {
+        // Redirect to the transaction form with a success message
+
+        $account_total = $account_total + $amount;
+        $updateQuery = "UPDATE users 
+                       SET account_total=?
+                       WHERE id = ?";
+
+        // Prepare the statement
+        $stmt = $conn->prepare($updateQuery);
+
+        // Bind parameters
+        $stmt->bind_param("ii", $account_total, $accountId);
+
+        // Execute the statement
+        if ($stmt->execute()) {
+            // Successful update
+            // Update the session variable for account_total
+            $_SESSION['account_total'] = $account_total;
+            
+            // Redirect to the transaction form with a success message           
+            header("Location: home.php?success=Bill paid to $amount_to");
+            exit();
+        } else {
+            // Error during update
+            header("Location: home.php?error=Error adding transaction. Please try again.");
+            exit();
+        }
+    } else {
+        // Redirect to the transaction form with an error message
+        header("Location: home.php?error=Error adding transaction. Please try again.");
+        exit();
+    }
+}
+
+
